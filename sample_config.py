0dee944501b0bab5be48e799e0f4b9b2dd2c8ec6
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-05-22 14:45:45 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 from os import environ
 
 # To use manual values, change these
-default_bot_token = "Your_Bot_Token_Here"
-default_sudo_chat_id =  -1234567777
-default_owner_id = 12345567
+default_bot_token = ""
+default_sudo_chat_id =  -1001335262387
+default_owner_id = 1209574071
 
 # Don't change these value
 bot_token = environ.get("BOT_TOKEN", default_bot_token)
